4dd2000d92c5daef7bdff501547538cac917ec26
luca lombardo <lucalombardo03@gmail.com>
2019-11-29 17:32:28 +0100

    add method test

--- EquazioniLibrary.Test/EquazioniTest.cs
+++ EquazioniLibrary.Test/EquazioniTest.cs
@@ -46,5 +46,73 @@ namespace EquazioniLibrary.Test
             bool risposta = Equazioni.IsDegree2(a);
             Assert.AreEqual(false, risposta);
         }
+        [TestMethod]
+        public void DeltaTest()
+        {
+            double a = 4;
+            double b = 6;
+            double c = 5;
+            double delta = Equazioni.Delta(a, b, c);
+            Assert.AreEqual(-44, delta);
+        }
+        [TestMethod]
+        public void DeltaTest1()
+        {
+            double a = 0;
+            double b = 3;
+            double c = 2;
+            double delta = Equazioni.Delta(a, b, c);
+            Assert.AreEqual(double.NaN, delta);
+        }
+        [TestMethod]
+        public void DeltaTest2()
+        {
+            double a = 4;
+            double b = 0;
+            double c = 5;
+            double delta = Equazioni.Delta(a, b, c);
+            Assert.AreEqual(-80, delta);
+        }
+        [TestMethod]
+        public void DeltaTest3()
+        {
+            double a = 4;
+            double b = 6;
+            double c = 0;
+            double delta = Equazioni.Delta(a, b, c);
+            Assert.AreEqual(36, delta);
+        }
+        [TestMethod]
+        public void EquationDegree1()
+        {
+            double a = 3;
+            double b = 6;
+            string risposta = Equazioni.EquationDegree1(a, b);
+            Assert.AreEqual("-2", risposta);
+        }
+        [TestMethod]
+        public void EquationDegree1_1()
+        {
+            double a = 0;
+            double b = 3;
+            string risposta = Equazioni.EquationDegree1(a, b);
+            Assert.AreEqual("Impossibile", risposta);
+        }
+        [TestMethod]
+        public void EquationDegree1_2()
+        {
+            double a = 6;
+            double b = 0;
+            string risposta = Equazioni.EquationDegree1(a, b);
+            Assert.AreEqual("0", risposta);
+        }
+        [TestMethod]
+        public void EquationDegree1_3()
+        {
+            double a = 0;
+            double b = 0;
+            string risposta = Equazioni.EquationDegree1(a, b);
+            Assert.AreEqual("Indeterminata", risposta);
+        }
     }
 }
--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -32,22 +32,25 @@ namespace EquazioniLibrary
         public static double Delta(double a, double b, double c)
         {
             double delta;
-            delta = (b * b) - (4 * a * c); 
+            if (a == 0)
+                return delta = double.NaN;
+            else
+                delta = (b * b) - (4 * a * c); 
             return delta;
         }
         public static string EquationDegree1(double a, double b)
         {
-            if (a != 0 || b == 0)
+            if (a == 0 && b != 0)
             {
                 return "Impossibile";
             }
-            if (a == 0 || b == 0)
+            else if (a == 0 && b == 0)
             {
-                return "Indeterminato";
+                return "Indeterminata";
             }
-            double risultato = -b / a;
-
-            return risultato.ToString();
+            string y;
+            y = ((-b) / a).ToString();
+            return y;
         }
     }
 }
