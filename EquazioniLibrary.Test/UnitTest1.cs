d9724e53c058fa8fe6828de8dcc1e183f04502ae
luca lombardo <lucalombardo03@gmail.com>
2019-11-29 16:27:51 +0100

    Add project files.

---
+++ EquazioniLibrary.Test/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EquazioniLibrary;
+
+namespace EquazioniLibrary.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestEquazioni1()
+        {
+
+        }
+    }
+}
