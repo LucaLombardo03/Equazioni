c39e794b3533e33c518e31833bad3db26562c2ed
luca lombardo <lucalombardo03@gmail.com>
2019-12-01 00:26:22 +0100

    add equation2° degree

--- EquazioniLibrary/Equazioni.cs
+++ EquazioniLibrary/Equazioni.cs
@@ -35,8 +35,10 @@ namespace EquazioniLibrary
             if (a == 0)
                 return delta = double.NaN;
             else
-                delta = (b * b) - (4 * a * c); 
-            return delta;
+            {
+                delta = (b * b) - (4 * a * c);
+                return delta;
+            }  
         }
         public static string EquationDegree1(double a, double b)
         {
@@ -52,15 +54,42 @@ namespace EquazioniLibrary
             y = ((-b) / a).ToString();
             return y;
         }
-        public static string EquationDegree2(double a, double b, double c, double delta)
+        public static string EquationDegree2(double a, double b, double c)
         {
             string x1;
             string x2;
-            if (c == 0)
+            double delta = Equazioni.Delta(a, b, c);
+            if (b == 0 && c != 0)
+            {
+                x1 = ((Math.Sqrt(-c))/a).ToString();
+                x2 = (-(Math.Sqrt(-c)) / a).ToString();
+                return ($"x1 è {x1} e x2 è {x2}");
+            }
+            else if (b != 0 && c == 0)
             {
                 x1 = 0.ToString();
-                x2 = 
+                x2 = (-b / a).ToString();
+                return($"x1 è {x1} e x2 è {x2}");
             }
+            else if (a != 0 && b != 0 && c != 0)
+            {
+                if (delta > 0)
+                {
+                    x1 = (-b - Math.Sqrt(delta) / (2 * a)).ToString();
+                    x2 = (-b + Math.Sqrt(delta) / (2 * a)).ToString();
+                    return ($"x1 è {x1} e x2 è {x2}");
+                }
+                else if (delta == 0)
+                {
+                    x2 = x1 = (-b / (2 * a)).ToString();
+                    return ($"x1 è {x1} e x2 è {x2}");
+                }
+                else if (delta < 0)
+                {
+                    return "Impossibile";
+                }
+            }
+            return "";
         }
     }
 }
